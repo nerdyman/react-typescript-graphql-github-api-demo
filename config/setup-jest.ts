1a01da7b4f324458d114fcea9d66766c5aa29048
nerdyman <averynerdyman@gmail.com>
2019-08-04 14:04:33 +0100

    chore: add jest setup test and mock for fetch

---
+++ config/setup-jest.ts
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+import { GlobalWithFetchMock } from 'jest-fetch-mock';
+
+const customGlobal: GlobalWithFetchMock = global as GlobalWithFetchMock;
+customGlobal.fetch = require('jest-fetch-mock');
+customGlobal.fetchMock = customGlobal.fetch;
