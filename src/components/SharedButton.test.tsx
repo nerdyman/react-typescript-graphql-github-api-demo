ca1704a9e8b8f7135a725a2a55725767f869dd08
nerdyman <averynerdyman@gmail.com>
2019-08-06 10:26:38 +0100

    feat: add SharedButton styles

--- src/components/SharedButton.test.tsx
+++ src/components/SharedButton.test.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { ThemeProvider } from '../ThemeProvider';
+
 import { SharedButton } from './SharedButton';
 
 it('Should render without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<SharedButton>Test</SharedButton>, div);
+    ReactDOM.render(
+        <ThemeProvider>
+            <SharedButton>Test</SharedButton>
+        </ThemeProvider>,
+        div,
+    );
     ReactDOM.unmountComponentAtNode(div);
 });
--- src/components/SharedButton.tsx
+++ src/components/SharedButton.tsx
@@ -1,5 +1,42 @@
 import React from 'react';
 
-export const SharedButton: React.FC<
+import styled from '../utilities/styled';
+
+// import { SharedBox } from './SharedBox';
+
+/**
+ * Generic unstyled button
+ */
+export const SharedButtonRoot: React.FC<
     React.ButtonHTMLAttributes<HTMLButtonElement>
-> = props => <button {...props} />;
+> = props => <button tabIndex={0} {...props} />;
+
+/**
+ * Generic unstyled button with styled system props
+ */
+// const SharedButtonSystem = SharedBox.withComponent(SharedButtonRoot);
+
+/**
+ * Generic styled button
+ */
+export const SharedButton = styled(SharedButtonRoot)`
+    padding: ${props =>
+        `${props.theme.space.half} ${props.theme.space.threeQuarter}`};
+    border-radius: ${props => props.theme.radii[0]};
+    font-weight: ${props => props.theme.fontWeights.bases[3]};
+    background-color: ${props => props.theme.colors.brandPrimaryBase};
+    color: ${props => props.theme.colors.brandPrimaryContrast};
+    cursor: pointer;
+
+    &:focus {
+        box-shadow: 0 0 0 0.25rem
+            ${props => props.theme.colors.uiInteractiveOutlineBase};
+        outline-style: solid;
+        outline-offset: -1px;
+    }
+
+    &[disabled] {
+        cursor: not-allowed;
+        opacity: 0.65;
+    }
+`;
