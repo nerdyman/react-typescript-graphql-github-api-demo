655d45f9c6ecf39e5594979af074a85eef02f245
nerdyman <averynerdyman@gmail.com>
2019-08-09 04:46:29 +0100

    fix: fix route404 test

--- src/components/Route404.test.tsx
+++ src/components/Route404.test.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { ThemeProvider } from '../ThemeProvider';
+
 import { Route404 } from './Route404';
 
 it('Should render without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Route404 />, div);
+    ReactDOM.render(
+        <ThemeProvider>
+            <Route404 />
+        </ThemeProvider>,
+        div,
+    );
     ReactDOM.unmountComponentAtNode(div);
 });
